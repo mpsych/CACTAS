0e20322403dfe5407997bb435bf6dd3288930a54
Daniel Haehn <haehn@mpsych.org>
2022-10-17 22:37:39 -0400

    Zoom WIP.

--- js/viewer.js
+++ js/viewer.js
@@ -12,6 +12,10 @@ H.Viewer = function(element) {
   //dissable crosshair
   nv.setCrosshairColor([0, 0, 0, 0])
 
+  // TODO zoom?
+  // nv.setSliceMM = true;
+  // nv.opts.dragMode = nv.dragModes.pan;
+
   this.nv = nv;
 
 };
