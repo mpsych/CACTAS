066aac776dd7563c371c427b78231ccb479d1879
fib <0x0f0f@pm.me>
2023-01-02 22:05:26 -0500

    undo on 'Z' keypress

--- js/annotator3.js
+++ js/annotator3.js
@@ -155,3 +155,28 @@ H.Annotator.prototype.grow = function(i, j, k) {
     }
   }
 };
+
+// undo: 'Z' keypress
+addEventListener('keydown', (e) => {
+  if (e.code == "KeyZ") {
+    
+    let last_label = H.A.label_to_draw;
+
+    console.log(`undo label ${last_label}`);
+
+    for (let pt of H.A.labels[last_label]) {
+
+      let i, j, k;
+      [i, j, k] = pt;
+
+      H.A.setLabelmapPixel(i, j, k, 0);
+    }
+
+    H.V.v.refresh();
+
+    delete H.A.labels[last_label];
+
+    // to reuse the same label
+    H.D.label--;
+  }
+});
