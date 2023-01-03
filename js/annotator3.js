164e1b27cf16e1e91227813cb6acbb9c8d6a3422
fib <0x0f0f@pm.me>
2023-01-02 20:17:57 -0500

    implement label merging

--- js/annotator3.js
+++ js/annotator3.js
@@ -138,4 +138,17 @@ H.Annotator.prototype.grow = function(i, j, k) {
 
   console.log('Found labels to merge', this.labels_to_merge);
 
+  // merge labels
+  for (let label in this.labels_to_merge) {
+
+    // this.labels[this.label_to_draw].forEach(pt => {
+    for (let pt of this.labels[this.label_to_draw]) {
+
+      let i, j, k;
+
+      [i, j, k] = pt;
+
+      this.setLabelmapPixel(i, j, k, label);
+    }
+  }
 };
