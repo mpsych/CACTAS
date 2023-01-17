ceef86a3144f01e04ebacc76513aaad034bda78b
Daniel Haehn <haehn@mpsych.org>
2023-01-16 20:04:50 -0500

    Add Josh's compare script and some sample data.

---
+++ _EXPERIMENTS/compare.py
@@ -0,0 +1,39 @@
+import os
+import argparse
+
+import numpy as np
+import nrrd
+import nibabel as nib
+from sklearn import metrics
+
+
+def read_file(file):
+
+    ext = os.path.splitext(file)[-1][1:]
+
+    if ext == 'nrrd':
+        data = nrrd.read(file)
+    elif ext == 'nii':
+        data = nib.load(file)
+        # data =  nib.Nifti2Image.from_filename(file)
+    else:
+        data = None
+        print('unsupported.')
+
+    return data
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('truth')
+parser.add_argument('label')
+
+args = parser.parse_args()
+
+
+truth_data = read_file(args.truth)
+label_data = read_file(args.label)
+
+
+similarity = metrics.jaccard_score(truth_data.ravel(), label_data.ravel(), "micro")
+
+print('Jaccard:', similarity)
