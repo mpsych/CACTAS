03d10acaa37839284e2ecd4857fd94759dafe89a
fib <0x0f0f@pm.me>
2023-01-17 11:56:35 -0500

    add slice navigation using arrows

--- js/drawer.js
+++ js/drawer.js
@@ -70,7 +70,7 @@ H.Drawer.prototype.setupInteraction = function () {
   this.nv.canvas.onmousedown = this.onMouseDown.bind(this);
   this.nv.canvas.onmousemove = this.onMouseMove.bind(this);
   this.nv.canvas.onmouseup = this.onMouseUp.bind(this);
-  window.onkeypress = this.onKeyPress.bind(this);
+  window.onkeydown = this.onKeyDown.bind(this);
 
 };
 
@@ -125,7 +125,7 @@ H.Drawer.prototype.onMouseUp = function (e) {
 };
 
 
-H.Drawer.prototype.onKeyPress = function(e) {
+H.Drawer.prototype.onKeyDown = function(e) {
 
   if (e.code == 'Space') {
     
@@ -139,9 +139,27 @@ H.Drawer.prototype.onKeyPress = function(e) {
 
     H.D.save();
 
+  } else if (e.code == 'ArrowLeft') {
+
+    this.nv.moveCrosshairInVox(-1, 0, 0);
+
+  } else if (e.code == 'ArrowRight') {
+
+    this.nv.moveCrosshairInVox(1, 0, 0);
+
+  } else if (e.code == 'ArrowDown') {
+
+    // anterior 
+    this.nv.moveCrosshairInVox(0, 1, 0);
+
+  } else if (e.code == 'ArrowUp') {
+
+    // posterior 
+    this.nv.moveCrosshairInVox(0, -1, 0);
+
   }
 
-};
+}
 
 
 H.Drawer.prototype.refresh = function() {
